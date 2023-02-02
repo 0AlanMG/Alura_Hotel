f73df32b754e66fdcf89df86c47fffeedf57e580
0AlanMG <esc.amdog5@gmail.com>
2023-02-02 17:16:44 -0600

    Create window

---
+++ src/com/alurahotel/views/Login.java
@@ -0,0 +1,39 @@
+package com.alurahotel.views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Login extends JFrame {
+	private JPanel window;
+	
+	// Show View
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	// Create Frame
+	public Login() {
+		// Window
+		setResizable(false);
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 788, 527);
+		window = new JPanel();
+		window.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(window);
+		window.setLayout(null);
+		setLocationRelativeTo(null);
+	}
+}
