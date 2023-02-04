ca1d7fec00bebbadcf1ebe0fa2d79ef6aa407d40
0AlanMG <esc.amdog5@gmail.com>
2023-02-03 20:53:20 -0600

    Menu y Header de Menu Usuario completos

--- src/com/alurahotel/view/MenuUsuario.java
+++ src/com/alurahotel/view/MenuUsuario.java
@@ -2,14 +2,25 @@ package com.alurahotel.view;
 
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
 import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class MenuUsuario extends JFrame {
 	private JPanel window;
+	int xMouse, yMouse;
+	private JLabel labelExit;
 	
 	// Show View
 	public static void main(String[] args) {
@@ -28,7 +39,8 @@ public class MenuUsuario extends JFrame {
 	// Create frame
 	public MenuUsuario() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(MenuUsuario.class.getResource("/com/alurahotel/view/images/aH-40px.png")));
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		// TODO Cambiar Codigo de salir
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 944, 609);
 		window = new JPanel();
 		window.setBackground(Color.WHITE);
@@ -38,5 +50,147 @@ public class MenuUsuario extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setUndecorated(true);
+		
+		// Window Header
+		JPanel header = new JPanel();
+		header.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				headerMouseDragged(e);
+			}
+		});
+		header.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				headerMousePressed(e);
+			}
+		});
+		
+		// Btn Exit / Close Window
+		JPanel btnExit = new JPanel();
+		btnExit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnExit.setBackground(Color.red);
+				labelExit.setForeground(Color.white);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnExit.setBackground(Color.white);
+			     labelExit.setForeground(Color.black);
+			}
+		});
+		
+		btnExit.setLayout(null);
+		btnExit.setBackground(Color.WHITE);
+		btnExit.setBounds(891, 0, 53, 36);
+		header.add(btnExit);
+		
+		labelExit = new JLabel("X");
+		labelExit.setBounds(0, 0, 53, 36);
+		labelExit.setHorizontalAlignment(SwingConstants.CENTER);
+		labelExit.setFont(new Font("Roboto", Font.PLAIN, 18));
+		btnExit.add(labelExit);
+		
+		// Window Menu
+		JPanel panelMenu = new JPanel();
+		panelMenu.setBackground(new Color(12, 138, 199));
+		panelMenu.setBounds(0, 0, 257, 609);
+		window.add(panelMenu);
+		panelMenu.setLayout(null);
+		
+		// Menu Logo
+		JLabel imageLogo = new JLabel("");
+		imageLogo.setBounds(50, 58, 150, 150);
+		panelMenu.add(imageLogo);
+		imageLogo.setIcon(new ImageIcon(MenuUsuario.class.getResource("/com/alurahotel/view/images/aH-150px.png")));
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(26, 219, 201, 2);
+		panelMenu.add(separator);
+		
+		header.setLayout(null);
+		header.setBackground(Color.WHITE);
+		header.setBounds(0, 0, 944, 36);
+		window.add(header);
+		
+		// Btn Registrar Reservas
+		JPanel btnRegistro = new JPanel();
+		btnRegistro.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnRegistro.setBackground(new Color(118, 187, 223));				
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnRegistro.setBackground(new Color(12, 138, 199));	
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO
+				/*ReservasView reservas = new ReservasView();
+				reservas.setVisible(true);
+				dispose();*/
+			}
+		});
+		btnRegistro.setBounds(0, 255, 257, 56);
+		btnRegistro.setBackground(new Color(12, 138, 199));
+		panelMenu.add(btnRegistro);
+		btnRegistro.setLayout(null);
+		
+		JLabel labelRegistro = new JLabel("Registro de reservas");
+		labelRegistro.setIcon(new ImageIcon(MenuUsuario.class.getResource("/com/alurahotel/view/images/reservado.png")));
+		labelRegistro.setForeground(SystemColor.text);
+		labelRegistro.setBounds(25, 11, 205, 34);
+		labelRegistro.setFont(new Font("Roboto", Font.PLAIN, 18));
+		labelRegistro.setHorizontalAlignment(SwingConstants.LEFT);
+		btnRegistro.add(labelRegistro);
+		
+		// Btn Busqueda de Reservas
+		JPanel btnBusqueda = new JPanel();
+		btnBusqueda.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnBusqueda.setBackground(new Color(118, 187, 223));				
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnBusqueda.setBackground(new Color(12, 138, 199));	
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO
+				/*Busqueda busqueda = new Busqueda();
+				busqueda.setVisible(true);
+				dispose();*/
+			}
+		});
+		btnBusqueda.setBounds(0, 312, 257, 56);
+		btnBusqueda.setBackground(new Color(12, 138, 199));
+		panelMenu.add(btnBusqueda);
+		btnBusqueda.setLayout(null);
+		
+		JLabel lblBusquedaDeReservas = new JLabel("Búsqueda");
+		lblBusquedaDeReservas.setIcon(new ImageIcon(MenuUsuario.class.getResource("/com/alurahotel/view/images/pessoas.png")));
+		lblBusquedaDeReservas.setBounds(27, 11, 200, 34);
+		lblBusquedaDeReservas.setHorizontalAlignment(SwingConstants.LEFT);
+		lblBusquedaDeReservas.setForeground(Color.WHITE);
+		lblBusquedaDeReservas.setFont(new Font("Roboto", Font.PLAIN, 18));
+		btnBusqueda.add(lblBusquedaDeReservas);
 	}
+	
+	// Content Panel Move
+	private void headerMousePressed(java.awt.event.MouseEvent evt) {
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }
+    private void headerMouseDragged(java.awt.event.MouseEvent evt) {
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x - xMouse, y - yMouse);
+    }
 }
