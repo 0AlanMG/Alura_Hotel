5a673897504d5b6874ec5e8ab8adfa5a980e7dbd
0AlanMG <esc.amdog5@gmail.com>
2023-02-04 18:34:27 -0600

    Header con movimiento de ventana

--- src/com/alurahotel/view/Reservas.java
+++ src/com/alurahotel/view/Reservas.java
@@ -1,8 +1,12 @@
 package com.alurahotel.view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.SystemColor;
 import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,6 +14,7 @@ import javax.swing.border.EmptyBorder;
 
 public class Reservas extends JFrame {
 	private JPanel window;
+	int xMouse, yMouse;
 	
 	// Show View
 	public static void main(String[] args) {
@@ -28,6 +33,7 @@ public class Reservas extends JFrame {
 	// Create Frame
 	public Reservas() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Reservas.class.getResource("/com/alurahotel/view/images/aH-40px.png")));
+		// TODO Modificar tipo de cerrado
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 910, 560);
 		setResizable(false);
@@ -39,5 +45,43 @@ public class Reservas extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setUndecorated(true);
+		
+		// Panel de Contenido
+		JPanel contentPanel = new JPanel();
+		contentPanel.setBorder(null);
+		contentPanel.setBackground(Color.WHITE);
+		contentPanel.setBounds(0, 0, 910, 560);
+		window.add(contentPanel);
+		contentPanel.setLayout(null);
+		
+		JPanel contentPanelHeader = new JPanel();
+		contentPanelHeader.setBounds(0, 0, 910, 36);
+		contentPanelHeader.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				headerMouseDragged(e);
+			     
+			}
+		});
+		contentPanelHeader.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				headerMousePressed(e);
+			}
+		});
+		contentPanelHeader.setLayout(null);
+		contentPanelHeader.setBackground(Color.WHITE);
+		contentPanel.add(contentPanelHeader);
 	}
+	
+	// Window Move
+	private void headerMousePressed(java.awt.event.MouseEvent evt) {
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }
+	private void headerMouseDragged(java.awt.event.MouseEvent evt) {
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x - xMouse, y - yMouse);
+    }
 }
